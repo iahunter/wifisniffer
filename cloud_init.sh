a08f35a7e6aba0d7b61738c8678f55917816627d
travis riesenberg <travisriesenberg@gmail.com>
2020-01-17 16:20:25 -0600

    change install.sh to cloud_init.sh

--- cloud_init.sh
+++ cloud_init.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# Change Desktop Resolution - Adjust your VM View to Scaled Mode Manually
+echo "Changing Resolution \n"
+xrandr --size 1920x1200
+
+echo "Adding Telecom to Sudoers \n"
+# Add telecom to sudoers with no password
+sudo cat << EOF >> /etc/sudoers
+
+# Telecom No password when sudo
+telecom ALL=(ALL) NOPASSWD:ALL
+EOF
+
+
+############################################
+echo "Installing Teamviewer \n"
+# Install Teamviewer
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo apt install ./teamviewer_amd64.deb -y
+
+
+# Install Git
+sudo apt install git -y
+
 ############################################
 
 echo "Installing Wireshark \n"
@@ -13,6 +36,8 @@ sudo apt-get install linssid -y
 # Install Git
 sudo apt install git -y
 
+############################################
+
 git clone https://github.com/iahunter/wifisniffer.git wifisniffer
 
 sudo chmod -R 755 wifisniffer
@@ -25,5 +50,4 @@ gsettings set org.gnome.desktop.background primary-color "#111111"
 gsettings set org.gnome.desktop.background picture-uri /home/telecom/wifisniffer/background.png
 
 echo "Install Complete"
-
 
